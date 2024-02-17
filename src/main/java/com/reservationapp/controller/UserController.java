0e6081168627a07ae801fe7cd22dd59893e53190
Umesh-Saunshi <umeshsaunshi12@gmail.com>
2024-02-17 18:55:05 +0530

    getAllUsers done

--- src/main/java/com/reservationapp/controller/UserController.java
+++ src/main/java/com/reservationapp/controller/UserController.java
@@ -1,14 +1,14 @@
 package com.reservationapp.controller;
 
+import com.reservationapp.entity.User;
 import com.reservationapp.payload.UserDto;
 import com.reservationapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/users")
@@ -17,14 +17,20 @@ public class UserController {
     private UserService userService;
 
     @Autowired
-    public UserController (UserService userService){
-        this.userService=userService;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     @PostMapping("/create")
-    public ResponseEntity<UserDto> createUser (@RequestBody UserDto userDto){
-       UserDto user = userService.createUser(userDto);
-       return new ResponseEntity<>(user , HttpStatus.CREATED);
+    public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto) {
+        UserDto user = userService.createUser(userDto);
+        return new ResponseEntity<>(user, HttpStatus.CREATED);
 
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<List<UserDto>> getAllUsers() {
+        List<UserDto> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
     }
 }
